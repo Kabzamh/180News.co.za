c00d50d6c3bc563780744d86712eb3391129b4c6
Kabzamh <kabzamh@gmail.com>
2026-09-13 23:29:46 +0200

    Delete components directory

--- components/StickyAdSlot.tsx
+++
@@ -1,8 +0,0 @@
-import { getAdForSlot } from "@/lib/ads";
-import { StickyAd } from "@/components/StickyAd";
-
-export async function StickyAdSlot() {
-  const ad = await getAdForSlot("sticky-mobile");
-  if (!ad) return null;
-  return <StickyAd id={ad.id} advertiser={ad.advertiser} headline={ad.headline} cta={ad.cta} />;
-}
