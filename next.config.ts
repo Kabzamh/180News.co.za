406b94e3ccbc8b209978cc3b696aa939057ad3e1
Kabzamh <kabzamh@gmail.com>
2026-09-13 21:33:20 +0200

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["pg"],
+  output: "standalone",
+};
 
 export default nextConfig;
